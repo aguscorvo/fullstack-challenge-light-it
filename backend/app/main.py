e724b9458726d41595604469cea17d5f6d71b13f
Agustina Corvo <agustinacorvo@gmail.com>
2024-06-26 22:40:41 -0300

    Configure CORS middleware for development

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.api.v1.api_v1 import api_router
 from app.db.base import init_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.title = settings.PROJECT_NAME
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # TODO: update this before deploying to production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api/v1")
 
